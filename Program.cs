33a6b0661d84e5ad2c6526a720a133c1dd068819
Udassar <udginsmail@gmail.com>
2023-06-20 13:27:36 +0300

    Gotovo2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-﻿Console.Write("Vvedite koordinatu A1: ");
-int a1 = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Vvedite koordinatu A2: ");
-int a2 = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Vvedite koordinatu B1: ");
-int b1 = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Vvedite koordinatu B2: ");
-int b2 = Convert.ToInt32(Console.ReadLine()!);
+﻿// Console.Write("Vvedite koordinatu A1: ");
+// int a1 = Convert.ToInt32(Console.ReadLine()!);
+// Console.Write("Vvedite koordinatu A2: ");
+// int a2 = Convert.ToInt32(Console.ReadLine()!);
+// Console.Write("Vvedite koordinatu B1: ");
+// int b1 = Convert.ToInt32(Console.ReadLine()!);
+// Console.Write("Vvedite koordinatu B2: ");
+// int b2 = Convert.ToInt32(Console.ReadLine()!);
 
 
-double ab = Math.Sqrt(Math.Pow((a1 - b1), 2) + Math.Pow((a2 - b2), 2));
+// double ab = Math.Sqrt(Math.Pow((a1 - b1), 2) + Math.Pow((a2 - b2), 2));
 
-Console.WriteLine($"{ab}");
+// Console.WriteLine($"Rasstoyanie mezhdu tochkami = {ab:f2}");
+
+
+Console.Write("Vvedite chislo: ");
+int num = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= num; i++)
+{
+    int sqr = Convert.ToInt32(Math.Pow(i, 2));
+    Console.WriteLine(sqr);
+}
